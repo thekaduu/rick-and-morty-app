c7ded6707248157ae1707370cdb6b902d8c9d868
Carlos Eduardo <carloseduardolbraz@gmail.com>
2022-06-29 16:07:32 -0300

    criar paleta de cores

---
+++ lib/styles/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color black = Color.fromRGBO(32, 35, 41, 1);
+  static Color white = Color.fromRGBO(255, 255, 255, 1);
+  static Color orange = Color.fromRGBO(255, 152, 2, 1);
+  static Color gray = Color.fromRGBO(60, 62, 68, 1);
+  static Color green = Color.fromRGBO(72, 208, 176, 1);
+  static Color red = Color.fromRGBO(252, 108, 109, 1);
+  static Color disabled = Color.fromRGBO(158, 158, 158, 1);
+}
